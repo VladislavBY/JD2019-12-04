424b4012695ea51ceefdf22c3d5f9d60e48400e2
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2020-03-04 12:21:22 +0300

    TaskC

--- src/by/it/popkov/calc/LongReport.java
+++ src/by/it/popkov/calc/LongReport.java
@@ -3,26 +3,26 @@ package by.it.popkov.calc;
 class LongReport implements Report{
     @Override
     public void writeHead() {
-
+        System.out.println("Long Head");
     }
 
     @Override
     public void writeTimeStart() {
-
+        System.out.println("Long TimeStart");
     }
 
     @Override
     public void writeTimeFinish() {
-
+        System.out.println("Long TimeFinish");
     }
 
     @Override
     public void writeOperation() {
-
+        System.out.println("Long Operation");
     }
 
     @Override
     public void writeErrors() {
-
+        System.out.println("Long Errors");
     }
 }
--- src/by/it/popkov/calc/ShortReport.java
+++ src/by/it/popkov/calc/ShortReport.java
@@ -3,26 +3,26 @@ package by.it.popkov.calc;
 class ShortReport implements Report {
     @Override
     public void writeHead() {
-
+        System.out.println("Short Head");
     }
 
     @Override
     public void writeTimeStart() {
-
+        System.out.println("Short TimeStart");
     }
 
     @Override
     public void writeTimeFinish() {
-
+        System.out.println("Short TimeFinish");
     }
 
     @Override
     public void writeOperation() {
-
+        System.out.println("Short Operation");
     }
 
     @Override
     public void writeErrors() {
-
+        System.out.println("Short Errors");
     }
 }
