848d327898b35c417470b1ffff854fbfec8cce23
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2020-03-04 12:19:11 +0300

    TaskC

--- src/by/it/popkov/calc/ReportConstructor.java
+++ src/by/it/popkov/calc/ReportConstructor.java
@@ -1,4 +1,16 @@
 package by.it.popkov.calc;
 
 class ReportConstructor {
+    private Report report;
+    void setReportType(String type){
+        if (type.equalsIgnoreCase("short")) report = new ShortReport();
+        else if (type.equalsIgnoreCase("long")) report = new LongReport();
+    }
+    void writeReport(){
+        report.writeHead();
+        report.writeTimeStart();
+        report.writeOperation();
+        report.writeErrors();
+        report.writeTimeFinish();
+    }
 }
