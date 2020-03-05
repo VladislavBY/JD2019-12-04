d58b1254a3d4d6f59aacd30c7eae9490252dd05f
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2020-03-05 15:48:03 +0300

    TaskC

--- src/by/it/popkov/calc/ReportConstructor.java
+++ src/by/it/popkov/calc/ReportConstructor.java
@@ -1,13 +1,12 @@
 package by.it.popkov.calc;
 
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 
 class ReportConstructor {
     private Report report;
     void setReportType(String type){
-        if (type.equalsIgnoreCase("short")) report = new ShortReport();
-        else if (type.equalsIgnoreCase("long")) report = new LongReport();
+        if (type.equalsIgnoreCase("short")) report = new ReportShort();
+        else if (type.equalsIgnoreCase("long")) report = new ReportLong();
     }
     void writeReport(LocalDateTime startTime, LocalDateTime finishTime, String logOperation, String logError){
         report.writeHead();
--- src/by/it/popkov/calc/ReportShort.java
+++ src/by/it/popkov/calc/ReportShort.java
@@ -7,45 +7,41 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 
-class LongReport implements Report {
+class ReportShort implements Report {
     @Override
     public void writeHead() {
-        System.out.println("===========");
-        System.out.println("Long Report");
-        System.out.println("===========");
+        System.out.println("Short Report");
     }
 
     @Override
     public void writeTimeStart(LocalDateTime startTime) {
-        System.out.println("Start time: " + startTime.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM)));
+        System.out.println("Start time: " + startTime.format(DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT)));
     }
 
     @Override
     public void writeTimeFinish(LocalDateTime finishTime) {
-        System.out.println("Finish time: " + finishTime.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM)));
+        System.out.println("Finish time: " + finishTime.format(DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT)));
     }
 
     @Override
     public void writeOperation(String logOperation) {
-        System.out.println("-----------------------------");
         System.out.println("List of operation:");
         try {
             Files.lines(Paths.get(logOperation)).forEach(System.out::println);
         } catch (IOException e) {
             System.out.println("No operation");
         }
-        System.out.println("-----------------------------");
+
     }
 
     @Override
     public void writeErrors(String logError) {
-        System.out.println("-----------------------------");
         System.out.println("List of errors:");
         try {
             Files.lines(Paths.get(logError)).forEach(System.out::println);
         } catch (IOException e) {
             System.out.println("No errors :-)");
         }
-        System.out.println("-----------------------------");
+
     }
 }
